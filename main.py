1dc2034c6ef8b69eaedd8cc08bd26095518b7908
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-08-23 23:43:00 +0300

    update

--- main.py
+++ main.py
@@ -6,20 +6,16 @@ from training import train_model
 
 
 
-def main(audio_dir,text_dir,summary_dir):
-      # Example usage
+def main():
    model = SpeechToTextSummarizer()
    optimizer = setup_training(model)
    checkpoint_folder = 'checkpoint'
    load_model(model, optimizer, folder_path=checkpoint_folder)
    num_epochs = 10  
-   data_loaders,_=get_data_loaders(audio_dir,text_dir,summary_dir)
+   data_loaders,_=get_data_loaders()
    train_model(model, data_loaders, num_epochs, checkpoint_folder=checkpoint_folder)
  
 
 if __name__ == "__main__":
-      audio_dir="data//audio"
-      text_dir="data//text"
-      summary_dir="data//summary"
-      main(audio_dir,text_dir,summary_dir)
+      main()
     
